88bccde5ce3169315b570cd9628c67ca32d875ba
Stick1000 <janleymolina@outlook.com>
2020-10-29 16:59:43 +0800

    Added new files: conn.php - to connect with the database header.php - contains the stylesheets such as Bootstrap and jQuery

---
+++ src/header.php
@@ -0,0 +1,21 @@
+<?php include('conn.php'); ?>
+<!DOCTYPE html>
+<html>
+
+<head>
+	<title>The Eatery</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+	<!-- Stylesheets -->
+	<link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
+	<link href="open-iconic/font/css/open-iconic-bootstrap.css" rel="stylesheet">
+	<link href="customization.css" rel="stylesheet">
+
+	<!-- Favicon -->
+	<link rel= "icon" href="branding/favicon.png">
+
+	<!-- Bootstrap JS, jQuery, and Popper -->
+	<script src="js/jquery-3.5.1.slim.min.js"></script>
+	<script src="js/popper.min.js"></script>
+	<script src="bootstrap/js/bootstrap.min.js"></script>
+</head>
