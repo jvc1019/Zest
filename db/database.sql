d284bb53f6f2d8d1e8c6a727ab4ddc1fe96e99a2
Stick1000 <janleymolina@outlook.com>
2020-10-29 16:53:16 +0800

    Edited database.sql

--- db/database.sql
+++ db/database.sql
@@ -3,10 +3,10 @@
 --
 
 CREATE TABLE `user` (
-  `user_ID` int(3) ZEROFILL NOT NULL AUTO_INCREMENT,
-  `user_Name` varchar(15) NOT NULL,
-  `user_Password` varchar(100) NOT NULL,
-  `user_Theme` varchar(50) NOT NULL DEFAULT "default",
+  `user_ID` int(3) ZEROFILL NOT NULL AUTO_INCREMENT, -- supports up to 999 users
+  `user_Name` varchar(30) NOT NULL,
+  `user_Password` varchar(100) NOT NULL, -- before placed here, the string is hashed via MD5
+  `user_Theme` varchar(50) NOT NULL DEFAULT "default", -- stores the theme name of the user
   PRIMARY KEY (`user_ID`),
   UNIQUE KEY `user_Name` (`user_Name`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
@@ -18,7 +18,7 @@ CREATE TABLE `user` (
 --
 
 CREATE TABLE `task` (
-  `task_ID` int(4) ZEROFILL NOT NULL AUTO_INCREMENT,
+  `task_ID` int(4) ZEROFILL NOT NULL AUTO_INCREMENT, -- supports up to 9999 tasks
   `task_Name` varchar(250) DEFAULT NULL,
   `task_Desc` longtext DEFAULT NULL,
   `task_Due` datetime DEFAULT NULL,
@@ -39,7 +39,7 @@ CREATE TABLE `task` (
 
 CREATE TABLE `subject` (
   `subject_ID` int(2) ZEROFILL NOT NULL AUTO_INCREMENT,
-  `subject_Icon` varchar(1) DEFAULT NULL, -- show an emoji picker when adding or editing a subject
+  `subject_Icon` varchar(1) DEFAULT NULL, -- show an emoji picker when adding or editing a subject, serves as icon
   `subject_Name` varchar(150) DEFAULT NULL,
   `subject_Type` varchar(3) NOT NULL DEFAULT "LEC", -- just add a dropdown to select a subject type adding or editing a subject
   `subject_Instructor` varchar(100) DEFAULT NULL,
@@ -59,7 +59,7 @@ CREATE TABLE `subject` (
 --
 
 CREATE TABLE `note` (
-  `note_ID` int(2) ZEROFILL NOT NULL AUTO_INCREMENT,
+  `note_ID` int(2) ZEROFILL NOT NULL AUTO_INCREMENT, -- up to 99 notes
   `note_Title` varchar(150) DEFAULT NULL,
   `note_Content` longtext DEFAULT NULL,
   `note_Tags` text DEFAULT NULL,
