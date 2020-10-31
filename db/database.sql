85405a9dd093ca4abb7745ec54e7a7cb65f1f632
Stick1000 <janleymolina@outlook.com>
2020-10-31 16:02:00 +0800

    Updated database.sql

--- db/database.sql
+++ db/database.sql
@@ -20,10 +20,9 @@ CREATE TABLE `user` (
 CREATE TABLE `task` (
   `task_ID` int(4) ZEROFILL NOT NULL AUTO_INCREMENT, -- supports up to 9999 tasks
   `task_Name` varchar(250) DEFAULT NULL,
-  `task_Desc` longtext DEFAULT NULL,
+  `task_Desc` mediumtext DEFAULT NULL,
   `task_Due` date DEFAULT NULL,
-  `task_Reminder_Date` date DEFAULT NULL,
-  `task_Reminder_Time` time DEFAULT NULL,
+  `task_Reminder_Date` datetime DEFAULT NULL, -- added support for the reminder feature
   `task_isDone` tinyint(1) NOT NULL DEFAULT 0,
   `task_Tags` varchar(250) DEFAULT NULL,
   `user_ID` int(3) ZEROFILL NOT NULL,
@@ -44,7 +43,7 @@ CREATE TABLE `subject` (
   `subject_Name` varchar(150) DEFAULT NULL,
   `subject_Type` varchar(3) NOT NULL DEFAULT "LEC", -- just add a dropdown to select a subject type adding or editing a subject
   `subject_Instructor` varchar(100) DEFAULT NULL,
-  `subject_Desc` longtext DEFAULT NULL,
+  `subject_Desc` mediumtext DEFAULT NULL,
   `subject_Day` varchar(8) DEFAULT NULL,
   `subject_Time` time DEFAULT NULL,
   `user_ID` int(3) ZEROFILL NOT NULL,
@@ -61,11 +60,26 @@ CREATE TABLE `subject` (
 
 CREATE TABLE `note` (
   `note_ID` int(2) ZEROFILL NOT NULL AUTO_INCREMENT, -- up to 99 notes
-  `note_Title` varchar(150) DEFAULT NULL,
-  `note_Content` longtext DEFAULT NULL,
+  `note_Title` varchar(250) DEFAULT NULL,
+  `note_Content` mediumtext DEFAULT NULL,
   `note_Tags` text DEFAULT NULL,
   `user_ID` int(3) ZEROFILL NOT NULL,
   PRIMARY KEY (`note_ID`),
   UNIQUE KEY `note_Title` (`note_Title`),
   FOREIGN KEY (`user_ID`) REFERENCES `user`(`user_ID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+
+-- These are dummy values to prevent errors with AUTO_INCREMENT,
+-- therefore the lowest possible ID for user-generated entries is 2
+INSERT INTO `user` (`user_ID`, `user_Name`, `user_Password`) VALUES
+(1, 'superuser', 'password');
+
+INSERT INTO `task` (`task_ID`, `task_Name`, `task_Desc`, `user_ID`) VALUES
+(1, 'Dummy task', 'Dummy task for AUTO_INCREMENT, DO NOT DELETE!', 1);
+
+INSERT INTO `subject` (`subject_ID`, `subject_Name`, `subject_Desc`, `user_ID`) VALUES
+(1, 'Dummy subject', 'Dummy subject for AUTO_INCREMENT, DO NOT DELETE!', 1);
+
+INSERT INTO `note` (`note_ID`, `note_Title`, `note_Content`, `user_ID`) VALUES
+(1, 'Dummy note', 'Dummy note for AUTO_INCREMENT, DO NOT DELETE!', 1);
