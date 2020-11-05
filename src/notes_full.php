33a46e82f71941895cb3ed6d82c7cd478d12d381
gracielouj <gracieloujomoc12@gmail.com>
2020-11-05 23:57:28 +0800

    Edited notes_full.php

--- src/notes_full.php
+++ src/notes_full.php
@@ -1,30 +1,18 @@
+<?php include('header.php'); ?>
+    <body>
+        <div class="mx-auto" style="width: 1200px; margin-top: 50px">
+        <form method="POST" id="addnote" action="notes_add.php" enctype="multipart/form-data">
+            <input type="text" name="note_Title" placeholder="Note Title" required>  
+            <input type=" text" name="note_Tags" placeholder="Tags (separated by a comma)">
+            <textarea name="note_Content" id="note_Content" rows="10" cols="80">Input note content here</textarea>
+            <input type="text" name="user_ID" value=<?php echo $user_ID; ?> hidden>
+            
+            <button type="button" class="btn btn-sm text-secondary" data-dismiss="modal">Cancel</button>
+            <button type="submit" name="submit" class="btn btn-sm text-primary">Save</button>            
+        </form>
+        </div>
 
-<html lang="en">
-  <head>
-    <meta charset="UTF-8">
-    <title>Notebook editor</title>
-
-    <script src="ckeditor/ckeditor.js"></script>
-
-
-  </head>
-  <body>
-
-    <form method="post" action="notes_add.php">
-    <textarea name = "title" id= "note_Title"></textarea> <!-- please change title holder-->
-    <textarea name = "tags" id= "note_Tags"></textarea>
-    <textarea name="content" id="content" rows="10" cols="80">
-    Input note content here
-    </textarea>
-    <input type="submit" name="submit" value="SUBMIT">
-    </form>
-
-
-</form>
-
-      <script>
-      CKEDITOR.replace('content');
-      </script>
-
-  </body>
-</html>
+        <script>
+            CKEDITOR.replace('note_Content');
+        </script>
+    </body>
