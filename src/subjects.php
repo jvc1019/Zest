6c7f1fe231b37b784f45b9a93d4c63f92f4d585d
adriel <jmrabe@up.edu.ph>
2020-11-09 20:27:12 +0800

    Added some includes. Nothing much changed yet

--- src/subjects.php
+++ src/subjects.php
@@ -1,2 +1,13 @@
 <!--php file responsible for adding, editing, maintaining and deleting subjects-->
 <!--To be worked on by Mico and Jett-->
+
+<?php
+    include("header.php");
+    include("user_details.php");
+
+
+?>
+
+<body>
+
+</body>
