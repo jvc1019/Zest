aeaf1e1001995b61cbe7af51628c60d93d5e2618
adriel <jmrabe@up.edu.ph>
2020-11-11 15:21:21 +0800

    Added cards to display subjects. Still work in progress

--- src/subjects.php
+++ src/subjects.php
@@ -2,6 +2,7 @@
 <!--To be worked on by Mico and Jett-->
 
 <body>
+    <div class="container">
     <?php
         include("header.php");
         // include("user_details.php");
@@ -16,12 +17,33 @@
             
                 //store to array named subjects         
                 while ($subjects = mysqli_fetch_assoc($result)){
-                //just to check what we are dealing with
-                //     print_r($subjects);
+                    //just to check what we are dealing with
+                    // print_r($subjects);
+    ?>
+                    <!--Cards Section-->
+                    <div class="col-sm-4 col-md-3">
+                        <div class="card" style="width: 16rem;">
+                            <img class="card-img-top" src="img/something.jpg" alt="Image here/Maybe emoji">
+                            <div class="card-body">
+                                <h4 class="card-title"><?php echo $subjects['subject_Name']?></h4>
+                                <h5 class="card-subtitle mb-2 text-muted"><?php echo $subjects['subject_Instructor']?></h5>
+                                <p class="card-text"><?php echo $subjects['subject_Desc']?></p>
+
+                            </div>
+                        
+                        
+                        </div>
+                    
+                    </div>
+
+    <?php
+                //end of while
                 }
 
-
+            //end of if (mysqli_num_rows($result)>0){
             }
+
+        //end of if ($result){
         }
 
     ?>
