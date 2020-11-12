132f120e5288571fb46b02b442eabf12c5b55863
adriel <jmrabe@up.edu.ph>
2020-11-12 13:50:43 +0800

    Updated subjects_add.php to store values after adding a subject from the modal

--- src/subjects_add.php
+++ src/subjects_add.php
@@ -1,2 +1,23 @@
 <!--used in adding new subjects-->
 <!--To be worked on by Mico and Jett-->
+
+<?php
+    include("header.php");
+    // include("user_details.php");
+
+    $sName = $_POST ["subjectName"];
+    $sType = $_POST ["subjectType"];
+    $sInstructor = $_POST ["subjectInstructor"];
+    $sDesc = $_POST ["subjectDesc"];
+    $sDay = $_POST ["subjectDay"];
+
+    //checking
+    // echo $sName;
+    // echo $sType;
+    // echo $sInstructor;
+    // echo $sDesc;
+    // echo $sDay;
+
+    // $subjectsql = "INSERT INTO "
+
+?>
