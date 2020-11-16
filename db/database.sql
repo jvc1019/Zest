afde7d08f364aa0496e7305bd642c6386b222717
Stick1000 <janleymolina@outlook.com>
2020-11-16 22:34:50 +0800

    Updated database.sql - Limited user_Email to 100 chars - Limited user_Password to 150 chars - Limited task_Name to 75 chars - Changed subject_Icon to subject_Image, to allow customization of subject cover images instead - Limited note_Title to 150 chars - Changed note_Tags data type to varchar with length 250 - Fixed length of subjects_Day length from 8 to 9 to accomodate Wednesdays - Added task_Tags value to dummy entry on 'task' table

--- db/database.sql
+++ db/database.sql
@@ -5,8 +5,8 @@
 CREATE TABLE `user` (
   `user_ID` int(3) ZEROFILL NOT NULL AUTO_INCREMENT, -- supports up to 999 users
   `user_Name` varchar(30) NOT NULL,
-  `user_Email` varchar(200) NOT NULL,
-  `user_Password` varchar(100) NOT NULL, -- before placed here, the string is hashed via MD5
+  `user_Email` varchar(100) NOT NULL,
+  `user_Password` varchar(125) NOT NULL, -- before placed here, the string is hashed via MD5
   `user_Theme` varchar(50) NOT NULL DEFAULT "default", -- stores the theme name of the user
   PRIMARY KEY (`user_ID`),
   UNIQUE KEY `user_Name` (`user_Name`),
@@ -21,7 +21,7 @@ CREATE TABLE `user` (
 
 CREATE TABLE `task` (
   `task_ID` int(4) ZEROFILL NOT NULL AUTO_INCREMENT, -- supports up to 9999 tasks
-  `task_Name` varchar(250) DEFAULT NULL,
+  `task_Name` varchar(75) DEFAULT NULL,
   `task_Desc` mediumtext DEFAULT NULL,
   `task_Due` date DEFAULT NULL,
   `task_Reminder` datetime DEFAULT NULL, -- added support for the reminder feature
@@ -40,12 +40,12 @@ CREATE TABLE `task` (
 
 CREATE TABLE `subject` (
   `subject_ID` int(2) ZEROFILL NOT NULL AUTO_INCREMENT, -- up to 99 subjects
-  `subject_Icon` varchar(1) DEFAULT NULL, -- show an emoji picker when adding or editing a subject, serves as icon
+  `subject_Image` varchar(200) DEFAULT NULL,
   `subject_Name` varchar(150) DEFAULT NULL,
   `subject_Type` varchar(3) NOT NULL DEFAULT "LEC", -- just add a dropdown to select a subject type adding or editing a subject
   `subject_Instructor` varchar(100) DEFAULT NULL,
   `subject_Desc` mediumtext DEFAULT NULL,
-  `subject_Day` varchar(8) DEFAULT NULL,
+  `subject_Day` varchar(9) DEFAULT NULL,
   `subject_Time` time DEFAULT NULL,
   `user_ID` int(3) ZEROFILL NOT NULL,
   PRIMARY KEY (`subject_ID`),
@@ -60,9 +60,9 @@ CREATE TABLE `subject` (
 
 CREATE TABLE `note` (
   `note_ID` int(2) ZEROFILL NOT NULL AUTO_INCREMENT, -- up to 99 notes
-  `note_Title` varchar(250) DEFAULT NULL,
+  `note_Title` varchar(150) DEFAULT NULL,
   `note_Content` mediumtext COLLATE utf8_unicode_ci NOT NULL,
-  `note_Tags` text DEFAULT NULL,
+  `note_Tags` varchar(250) DEFAULT NULL,
   `user_ID` int(3) ZEROFILL NOT NULL,
   PRIMARY KEY (`note_ID`),
   FOREIGN KEY (`user_ID`) REFERENCES `user`(`user_ID`)
@@ -74,8 +74,8 @@ CREATE TABLE `note` (
 INSERT INTO `user` (`user_ID`, `user_Name`, `user_Password`) VALUES
 (1, 'superuser', 'password');
 
-INSERT INTO `task` (`task_ID`, `task_Name`, `task_Desc`, `user_ID`) VALUES
-(1, 'Dummy task', 'Dummy task for AUTO_INCREMENT, DO NOT DELETE!', 1);
+INSERT INTO `task` (`task_ID`, `task_Name`, `task_Desc`, `task_Tags`, `user_ID`) VALUES
+(1, 'Dummy task', 'Dummy task for AUTO_INCREMENT, DO NOT DELETE!', 'dummytag1,dummytag2', 1);
 
 INSERT INTO `subject` (`subject_ID`, `subject_Name`, `subject_Desc`, `user_ID`) VALUES
 (1, 'Dummy subject', 'Dummy subject for AUTO_INCREMENT, DO NOT DELETE!', 1);
