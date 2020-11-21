d6b53430283a79e71917b6f880a6bbd8bdadf831
Stick1000 <janleymolina@outlook.com>
2020-11-22 00:56:15 +0800

    Moved timezone function from user_details.php to header.php

--- src/header.php
+++ src/header.php
@@ -1,4 +1,4 @@
-<?php include('conn.php'); ?>
+<?php include("conn.php"); ?>
 <!DOCTYPE html>
 <html>
 
@@ -13,11 +13,13 @@
 	<!-- Scripts -->
 	<script src="js/sidebar.js"></script>
 
-
 	<!-- Favicon -->
 	<link rel="icon" href="../resources/favicon.jpg"> <!-- Replace this with the proper icon -->
 
 	<!-- Bootstrap JS and jQuery-->
 	<script src="js/jquery-3.5.1.slim.min.js"></script>
 	<script src="bootstrap/js/bootstrap.bundle.min.js"></script>
+
+	<!-- Setting the default timezone for all PHP date() calls -->
+	<?php date_default_timezone_set("Asia/Manila"); ?>
 </head>
