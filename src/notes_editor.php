9cf084df9092c4e773b538acaade2c0db81d5c4f
gracielouj <gracieloujomoc12@gmail.com>
2020-11-22 21:02:50 +0800

    Resolves #32

--- src/notes_editor.php
+++ src/notes_editor.php
@@ -1,4 +1,5 @@
 <?php include('header.php'); ?>
+
 <!DOCTYPE html>
 
 <html lang="en">
@@ -10,33 +11,38 @@
 
 
 	<body>
+	<?php
+		$result = mysqli_query($conn,"SELECT * FROM note WHERE note_ID='" . $_GET['note_ID'] . "'");
+		$row= mysqli_fetch_array($result);
+	?>
 		<center><h1 style="padding: 20px"> Edit Note <h1></center>
 
-		<div class="container">
-			<form method="post" action="notes_edit.php" id="#editnote" enctype="multipart/form-data">
-                          
+		<div class="container" id="editnote">
+			<form method="POST" action="notes_edit.php" enctype="multipart/form-data">
+				<input type="text" name="note_ID" value="<?php echo $row['note_ID']?>" />
 				<!-- TITLE AND TAG TEXTBOXES -->
-				<div class="row mx-5 px-4 justify-content-left">
-					<input class="mr-2" type="text" name="note_Title" value="<?php echo $row['note_Title']; ?>">  
-            		<input class="ml-0" type=" text"  name="note_Tags" value="<?php echo $row['note_Tags']; ?>" placeholder="Tags (separated by a comma)" rows="3">
+				<div class="row mx-5 px-4 justify-content-center">
+					<input class="mr-2" type="text" name="note_Title" value="<?php echo $row['note_Title']?>">  
+            		<input class="ml-0" type=" text" name="note_Tags" value="<?php echo $row['note_Tags']?>">
 				</div>
 
 				<!-- RICHTEXT EDITOR -->
 				<div class="row justify-content-center my-1">
-					<textarea name="content" id="content" rows="10" cols="80" ><?php echo $row['note_Content']; ?></textarea>
+					<textarea name="note_Content" id="note_Content" rows="10" cols="80" ><?php echo $row['note_Content']?></textarea>
 				</div>
 
 				<div class="row justify-content-center my-1">
 					<button type="button" class="btn btn-sm text-secondary" data-dismiss="modal">Cancel</button>
             		<button type="submit" name="submit" class="btn btn-sm text-primary">Save</button>
 				</div>
+
 			</form>
 		<div>
 
 
 		<!-- FOR CKEDITOR -->
 		<script>
-    		CKEDITOR.replace('content', {
+    		CKEDITOR.replace('note_Content', {
       				extraPlugins: 'editorplaceholder',
       				editorplaceholder: 'Input note content here'
 
