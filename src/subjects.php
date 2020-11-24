60502887f210646e11388a9dbacb9e02d2c72b6f
adriel <jmrabe@up.edu.ph>
2020-11-24 22:35:48 +0800

    Edited some comments by just a bit

--- src/subjects.php
+++ src/subjects.php
@@ -1,5 +1,5 @@
 <!--php file responsible for adding, editing, maintaining and deleting subjects-->
-<!--To be worked on by Mico and Jett-->
+<!--To be worked on by Mico and Jett--> 
 <?php
 include("header.php");
 include("user_details.php");
