4a836663ec4ee6edd349f556463382f2e80d2b0b
Ronn <ronnianigharasvisto@gmail.com>
2020-11-25 19:35:10 +0800

    Edited the delete modal in notes_list.php for design consistency.

--- src/notes_list.php
+++ src/notes_list.php
@@ -47,17 +47,32 @@
                         <div class="modal-content">
 
                             <div class="modal-header">
-                                <h4 class="modal-title" id="myModalLabel">Delete Note</h4>
+                                <h5 class="modal-title text-danger" id="myModalLabel">Delete Note</h5>
                                 <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
                             </div>
 
                             <div class="modal-body">
-                                <h3 class="text-center">Delete <?php echo $row['note_Title']; ?>?</h3>
+                                <h5 class="text-center text-truncate"><?php echo "'$row[note_Title]'"; ?></h5>
+                                <p class="text-center">will be permanently deleted</p>
                             </div>
 
                             <div class="modal-footer">
-                                <button type="button" class="btn btn-sm text-secondary" data-dismiss="modal"> Cancel</button>
-                                <a href="notes_delete.php?note_ID=<?php echo $row['note_ID']; ?>" class="btn btn-sm text-danger"> Delete</a>
+                                <button type="button" class="btn btn-sm text-secondary" data-dismiss="modal">
+                                    <!-- x icon -->
+                                    <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-x-circle" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                                        <path fill-rule="evenodd" d="M8 15A7 7 0 1 0 8 1a7 7 0 0 0 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z" />
+                                        <path fill-rule="evenodd" d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z" />
+                                    </svg>
+                                    Cancel
+                                </button>
+                                <a href="notes_delete.php?note_ID=<?php echo $row['note_ID']; ?>" class="btn btn-sm text-danger">
+                                    <!-- trash bin icon -->
+                                    <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-trash" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                                        <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z" />
+                                        <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4L4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z" />
+                                    </svg>
+                                    Delete
+                                </a>
                             </div>
                         </div>
                     </div>
