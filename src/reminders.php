ce425c8980505aa6d0a6f3f8c65dbbecf9e97694
Jayvee <jayveecastaneda19@gmail.com>
2020-11-26 21:37:05 +0800

    Improved reminders at home page for better look

--- src/reminders.php
+++ src/reminders.php
@@ -3,41 +3,31 @@
 		<h5 class="remindTitle">DUE TODAY<h5>
 		<table>
 			<?php
-				$query = "SELECT * FROM task where user_ID = $user_ID and DATE(task_Due) = CURDATE()";
+				$query = "SELECT * FROM task WHERE user_ID = $user_ID AND DATE(task_Due) = CURDATE() ORDER BY task_Due";
 				$result = $conn->query($query);
 
 				if (!($result->num_rows > 0)) {
-	            	echo "<div class='notasksdue'>No tasks due today.</div>";
-	        	} else {
-	        		while ($row = $result->fetch_assoc()) {
-	        			if (intval(substr($row['task_Due'], 11, 2)) < 10){
-	        				$time = " " . substr($row['task_Due'], 12, 4) . " A.M.";
+					echo "<div class='notasksdue'>No tasks due today.</div>";
+				} else {
+					while ($row = $result->fetch_assoc()) {
+						if (strlen($row['task_Name']) > 25){
+	        				$name = substr($row['task_Name'], 0, 25) . "...";
+	        			}else{
+	        				$name = $row['task_Name'];
 	        			}
-	        			else if (intval(substr($row['task_Due'], 11, 2)) == 12){
-	        				$time = substr($row['task_Due'], 11, 5) . " P.M.";
-						}
-						else if (intval(substr($row['task_Due'], 11, 2)) > 12){
-	        				$time = (intval(substr($row['task_Due'], 11, 2)) - 12);
-	        				if ($time < 10){
-	        					$time = " " . substr($time, 0) . substr($row['task_Due'], 13, 3) . " P.M.";
-	        				}
-	        				else{
-	        					$time = (string)$time . substr($row['task_Due'], 13, 3) . " P.M.";
-	        				}
-						}
-						else{
-							$time = substr($row['task_Due'], 11, 5) . " A.M.";
-						}
-	        		?>
-	        		<tr class="center">
-	        			<td>
-	        				<div class="remindText"> 📝 <a href="tasks.php"><?php echo $row['task_Name'];?></a></div></td><td><div class="remindText"><pre>		</pre><?php echo $time?></div>
-	        			</td>
-	        		</tr>
-	        		<?php
-	        		}
-	        	}       	
-	        ?>
+					?>
+					<tr class="remindText">
+						<td class="col-md-3">
+							📝 <a href="tasks.php"><?php echo $name;?></a>
+						</td>
+						<td class="col-md-1">
+							<?php echo date("h:iA", strtotime($row['task_Due']));?></div>
+						</td>
+					</tr>
+					<?php
+					}
+				}
+			?>
 		</table>
 	</div>
 	<br>
@@ -45,38 +35,28 @@
 		<h5 class="remindTitle">DUE TOMORROW<h5>
 		<table>
 			<?php
-				$query = "SELECT * FROM task where user_ID = $user_ID and DATE(task_Due) = CURDATE() + INTERVAL 1 DAY";
+				$query = "SELECT * FROM task WHERE user_ID = $user_ID AND DATE(task_Due) = CURDATE() + INTERVAL 1 DAY ORDER BY task_Due";
 				$result = $conn->query($query);
 
 				if (!($result->num_rows > 0)) {
 	            	echo "<div class='notasksdue'>No tasks due tomorrow.</div>";
 	        	} else {
 	        		while ($row = $result->fetch_assoc()) {
-	        			if (intval(substr($row['task_Due'], 11, 2)) < 10){
-	        				$time = " " . substr($row['task_Due'], 12, 4) . " A.M.";
+	        			if (strlen($row['task_Name'])> 25){
+	        				$name = substr($row['task_Name'], 0, 25) . "...";
+	        			}else{
+	        				$name = $row['task_Name'];
 	        			}
-	        			else if (intval(substr($row['task_Due'], 11, 2)) == 12){
-	        				$time = substr($row['task_Due'], 11, 5) . " P.M.";
-						}
-						else if (intval(substr($row['task_Due'], 11, 2)) > 12){
-	        				$time = (intval(substr($row['task_Due'], 11, 2)) - 12);
-	        				if ($time < 10){
-	        					$time = " " . substr($time, 0) . substr($row['task_Due'], 13, 3) . " P.M.";
-	        				}
-	        				else{
-	        					$time = (string)$time . substr($row['task_Due'], 13, 3) . " P.M.";
-	        				}
-						}
-						else{
-							$time = substr($row['task_Due'], 11, 5) . " A.M.";
-						}
-	        		?>
-	        		<tr class="center">
-	        			<td>
-	        				<div class="remindText"> 📝 <a href="tasks.php"><?php echo $row['task_Name'];?></a></div></td><td><div class="remindText"><pre>		</pre><?php echo $time?></div>
-	        			</td>
-	        		</tr>
-	        		<?php
+					?>
+					<tr class="remindText">
+						<td class="col-md-8">
+							📝 <a href="tasks.php"><?php echo $name;?></a>
+						</td>
+						<td class="col-md-4">
+							<?php echo date("h:iA", strtotime($row['task_Due']));?></div>
+						</td>
+					</tr>
+					<?php
 	        		}
 	        	}       	
 	        ?>
