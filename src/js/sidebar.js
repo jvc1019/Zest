8d140c4dd947ec054f65dd9d3839902e8a31097b
Candyl John Faeldonea <cffaeldonea@up.edu.ph>
2020-11-26 15:07:15 +0800

    Fixes #12

--- src/js/sidebar.js
+++ src/js/sidebar.js
@@ -4,16 +4,10 @@ function toggleSidebar() {
   if (mini) {
     console.log("opening sidebar");
     document.getElementById("mySidebar").style.width = "250px";
-/*    document.getElementById("main").style.marginLeft = "250px";*/
     this.mini = false;
   } else {
     console.log("closing sidebar");
     document.getElementById("mySidebar").style.width = "90px";
-/*    document.getElementById("main").style.marginLeft = "85px";*/
     this.mini = true;
   }
-}
-function adjustContainer(){
-	console.log("adjust container");
-	document.getElementsByClassName('container').style.marginLeft = "90px";
 }
