000dcfffb2102c15a2b3270495b0e0456448c5bb
Jayvee <jayveecastaneda19@gmail.com>
2020-11-28 22:26:05 +0800

    Applied Avatar in Nav-bar

--- src/navbar.php
+++ src/navbar.php
@@ -33,11 +33,7 @@
          <?php
             if (isset($user_Name)) {
                 echo "<a href='profile.php'>
-        <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 16 16' class='bi bi-person-circle nav-icon'>
-            <path d='M13.468 12.37C12.758 11.226 11.195 10 8 10s-4.757 1.225-5.468 2.37A6.987 6.987 0 0 0 8 15a6.987 6.987 0 0 0 5.468-2.63z'/>
-            <path fill-rule='evenodd' d='M8 9a3 3 0 1 0 0-6 3 3 0 0 0 0 6z'/>
-            <path fill-rule='evenodd' d='M8 1a7 7 0 1 0 0 14A7 7 0 0 0 8 1zM0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8z'/></svg>
-
+            <img class='nav-icon' src='/cmsc128/resources/avatars/" . $user_Avatar . ".jpg'>
             <span class='icon-text'> $user_Name</span></a>
             </a>
             <a href='logout.php'>
@@ -58,7 +54,7 @@
             ?>
      </span>
      <!-- Help and Support Page - Please Change Icon and Position. Thanks! -->
-     <a href="help.php">
+     <a href="help.php?help=need">
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" class="bi bi-circle-square nav-icon">
              <path d="M0 6a6 6 0 1 1 12 0A6 6 0 0 1 0 6z" />
              <path d="M12.93 5h1.57a.5.5 0 0 1 .5.5v9a.5.5 0 0 1-.5.5h-9a.5.5 0 0 1-.5-.5v-1.57a6.953 6.953 0 0 1-1-.22v1.79A1.5 1.5 0 0 0 5.5 16h9a1.5 1.5 0 0 0 1.5-1.5v-9A1.5 1.5 0 0 0 14.5 4h-1.79c.097.324.17.658.22 1z" />
