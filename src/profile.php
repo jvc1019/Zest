823d03d856b9aeecf90932d85098a0fd25542d7a
Jayvee <jayveecastaneda19@gmail.com>
2020-11-29 17:05:31 +0800

    Changed look of profile.php and changed backgrounds.

--- src/profile.php
+++ src/profile.php
@@ -2,26 +2,27 @@
 <?php include("user_details.php") ?>
 <?php include('notification.php'); ?>
 
-<body>
+<body class="index-bg">
 	<!-- navigation bar -->
 	<?php include('sidebar.php'); ?>
-
-	<div class="container-fluid fullpage with-sidebar index-bg">
+	<div class="container-fluid fullpage with-sidebar">
 		<div class="row py-5">
 			<div class="card profileHolder mx-auto">
 				<?php echo "<img class='card-img-top' src='/cmsc128/resources/avatars/$user_Avatar.jpg'>" ?>
 				<div class="card-body">
 					<h2 class="text-center"><?php echo $user_Name ?></h2>
-					<p class="text-center"><?php echo $user_Email ?></p>
-					<?php
-					if (empty($user_Desc)) {
-						$user_Desc = "<div style='color:gray'>User has no description.</div>";
-					}
-					?>
-					<?php echo $user_Desc ?>
+					<p class="text-center font-italic"><?php echo $user_Email ?></p>
 					<button data-toggle="modal" data-target="#updateProfileModal<?php echo $user['user_ID']; ?>" class="btn btn-sm btn-primary btn-block">Edit Profile</button>
+					<br>
+					<div>
+						<?php
+						if (empty($user_Desc)) {
+							$user_Desc = "<div style='color:gray'>User has no description.</div>";
+						}
+						echo $user_Desc;
+						?>
+					</div>
 				</div>
-
 			</div>
 		</div>
 		<?php include("profile_modal.php"); ?>
