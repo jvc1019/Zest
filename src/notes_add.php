d2668194bfd663051dfa56a8238792d30c88916a
gracielouj <gracieloujomoc12@gmail.com>
2020-12-02 02:15:36 +0800

    Minor changes to add tags with quotation

--- src/notes_add.php
+++ src/notes_add.php
@@ -3,7 +3,7 @@ include('conn.php');
 
 $note_Title = "'" . trim(htmlentities($_POST['note_Title'], ENT_QUOTES)) . "'";
 $note_Content = !empty($_POST['note_Content']) ? "'" . $_POST['note_Content'] . "'" : "NULL";
-$note_Tags = !empty($_POST['note_Tags']) ? "'" . $_POST['note_Tags'] . "'" : "NULL";
+$note_Tags = !empty(trim(htmlentities($_POST['note_Tags'], ENT_QUOTES))) ? "'" . implode(",", array_unique(explode(",", str_replace($whitespace, "", trim(htmlentities($_POST['note_Tags'], ENT_QUOTES)))))) . "'" : "NULL";
 $user_ID = is_numeric($_POST['user_ID']) ? $_POST['user_ID'] : "NULL";
 
 $query = "INSERT INTO `note` (`note_Title`, `note_Content`, `note_Tags`, `user_ID`) VALUES ($note_Title, $note_Content, $note_Tags, $user_ID)";
