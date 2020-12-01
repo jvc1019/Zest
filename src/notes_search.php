fa4f41a66a51c0602a96b3e0e4a5707750526009
Stick1000 <janleymolina@outlook.com>
2020-12-01 21:25:10 +0800

    Update notes_search.php
    Co-Authored-By: gracielouj <44961839+gracielouj@users.noreply.github.com>
    Co-Authored-By: kirlsoo <44961919+kirlsoo@users.noreply.github.com>

--- src/notes_search.php
+++ src/notes_search.php
@@ -1,85 +1,85 @@
 <div id="notes_search">
     <div class="card-deck fourcolumns">
-<?php
-    require_once "conn.php";
-    include('user_details.php');
+        <?php
+        require_once("conn.php");
+        include('user_details.php');
 
-    if (isset($_POST['search'])) {
-     
-    $search = "SELECT * FROM note LEFT JOIN user ON note.user_ID=user.user_ID WHERE note.user_ID=$user_ID AND note_Title LIKE '{$_POST['search']}%' LIMIT 100";
-    $result = mysqli_query($conn, $search);
-    
-    if (mysqli_num_rows($result) > 0) {
-        while ($row = mysqli_fetch_array($result)) {?>
-                <div class="card" style="height: 16em;">
-                    <div class="card-body overflow-hidden">
-                        <h5><?php echo $row['note_Title']; ?></h5>
-                        <p class="text-muted"><?php echo '🏷 '.$row['note_Tags']; ?></p>
-                        <?php echo $row['note_Content']; ?>
-                    </div>
-                    <div class="card-footer text-center">
-                        <a href="notes_editor.php?note_ID=<?php echo $row['note_ID']; ?>" id="#editnote" class="btn text-primary btn-sm">
-                            <span>
-                                <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-info-circle" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-                                    <path fill-rule="evenodd" d="M8 15A7 7 0 1 0 8 1a7 7 0 0 0 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z" />
-                                    <path d="M8.93 6.588l-2.29.287-.082.38.45.083c.294.07.352.176.288.469l-.738 3.468c-.194.897.105 1.319.808 1.319.545 0 1.178-.252 1.465-.598l.088-.416c-.2.176-.492.246-.686.246-.275 0-.375-.193-.304-.533L8.93 6.588z" />
-                                    <circle cx="8" cy="4.5" r="1" />
-                                </svg>
-                            </span>
-                            Details
-                        </a>
-                        <a href="#notedelete<?php echo $row['note_ID']; ?>" data-toggle="modal" class="btn text-danger btn-sm">
-                            <span>
-                                <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-trash" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-                                    <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z" />
-                                    <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4L4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z" />
-                                </svg>
-                            </span>
-                            Delete
-                        </a>
-                    </div>
-                </div>
+        if (isset($_POST['search'])) {
 
-                <!-- Delete Note -->
-                <div class="modal fade" id="notedelete<?php echo $row['note_ID']; ?>" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
-                    <div class="modal-dialog">
-                        <div class="modal-content">
+            $search = "SELECT * FROM note LEFT JOIN user ON note.user_ID=user.user_ID WHERE note.user_ID=$user_ID AND note_Title LIKE '{$_POST['search']}%' LIMIT 100";
+            $result = $conn->query($search);
 
-                            <div class="modal-header">
-                                <h5 class="modal-title text-danger" id="myModalLabel">Delete Note</h5>
-                                <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-                            </div>
-
-                            <div class="modal-body">
-                                <h5 class="text-center text-truncate"><?php echo "'$row[note_Title]'"; ?></h5>
-                                <p class="text-center">will be permanently deleted</p>
-                            </div>
-
-                            <div class="modal-footer">
-                                <button type="button" class="btn btn-sm text-secondary" data-dismiss="modal">
-                                    <!-- x icon -->
-                                    <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-x-circle" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+            if ($result->num_rows > 0) {
+                while ($row = $result->fetch_assoc()) { ?>
+                    <div class="card" style="height: 16em;">
+                        <div class="card-body overflow-hidden">
+                            <h5><?php echo $row['note_Title']; ?></h5>
+                            <p class="text-muted"><?php echo '🏷 ' . $row['note_Tags']; ?></p>
+                            <?php echo $row['note_Content']; ?>
+                        </div>
+                        <div class="card-footer text-center">
+                            <a href="notes_editor.php?note_ID=<?php echo $row['note_ID']; ?>" id="#editnote" class="btn text-primary btn-sm">
+                                <span>
+                                    <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-info-circle" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
                                         <path fill-rule="evenodd" d="M8 15A7 7 0 1 0 8 1a7 7 0 0 0 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z" />
-                                        <path fill-rule="evenodd" d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z" />
+                                        <path d="M8.93 6.588l-2.29.287-.082.38.45.083c.294.07.352.176.288.469l-.738 3.468c-.194.897.105 1.319.808 1.319.545 0 1.178-.252 1.465-.598l.088-.416c-.2.176-.492.246-.686.246-.275 0-.375-.193-.304-.533L8.93 6.588z" />
+                                        <circle cx="8" cy="4.5" r="1" />
                                     </svg>
-                                    Cancel
-                                </button>
-                                <a href="notes_delete.php?note_ID=<?php echo $row['note_ID']; ?>" class="btn btn-sm text-danger">
-                                    <!-- trash bin icon -->
+                                </span>
+                                Details
+                            </a>
+                            <a href="#notedelete<?php echo $row['note_ID']; ?>" data-toggle="modal" class="btn text-danger btn-sm">
+                                <span>
                                     <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-trash" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
                                         <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z" />
                                         <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4L4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z" />
                                     </svg>
-                                    Delete
-                                </a>
+                                </span>
+                                Delete
+                            </a>
+                        </div>
+                    </div>
+
+                    <!-- Delete Note -->
+                    <div class="modal fade" id="notedelete<?php echo $row['note_ID']; ?>" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+                        <div class="modal-dialog">
+                            <div class="modal-content">
+
+                                <div class="modal-header">
+                                    <h5 class="modal-title text-danger" id="myModalLabel">Delete Note</h5>
+                                    <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+                                </div>
+
+                                <div class="modal-body">
+                                    <h5 class="text-center text-truncate"><?php echo "'$row[note_Title]'"; ?></h5>
+                                    <p class="text-center">will be permanently deleted</p>
+                                </div>
+
+                                <div class="modal-footer">
+                                    <button type="button" class="btn btn-sm text-secondary" data-dismiss="modal">
+                                        <!-- x icon -->
+                                        <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-x-circle" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                                            <path fill-rule="evenodd" d="M8 15A7 7 0 1 0 8 1a7 7 0 0 0 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z" />
+                                            <path fill-rule="evenodd" d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z" />
+                                        </svg>
+                                        Cancel
+                                    </button>
+                                    <a href="notes_delete.php?note_ID=<?php echo $row['note_ID']; ?>" class="btn btn-sm text-danger">
+                                        <!-- trash bin icon -->
+                                        <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-trash" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                                            <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z" />
+                                            <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4L4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z" />
+                                        </svg>
+                                        Delete
+                                    </a>
+                                </div>
                             </div>
                         </div>
                     </div>
-                </div>
-<?php    }
-  } else { ?>
-    <h6 class="text-center">No results found. Add some notes!</h6>
-<?php  }
- } ?>
+                <?php    }
+            } else { ?>
+                <h6 class="text-center">No results found. Add some notes!</h6>
+        <?php  }
+        } ?>
     </div>
 </div>
