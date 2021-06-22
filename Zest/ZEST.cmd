60b5cc5a93ce3b8be4cc4070f1d41dd1f1a4bc2d
Adriel <jmrabe@up.edu.ph>
2021-06-22 21:00:49 +0800

    Added Zest.cmd

---
+++ Zest/ZEST.cmd
@@ -0,0 +1,11 @@
+:::::::::::::::::::::::::::::::::::::::
+::      "ZEST.bat" version 1.0       ::
+:: By Ronn Ian Visto and Adriel Rabe ::
+::  Special thanks to Janley Molina  ::
+:::::::::::::::::::::::::::::::::::::::
+
+:: Runs apache_start to access the website without the need to open xampp_control 
+START C:\Zest\apache_start.bat 
+
+:: Opens index.php located at the address shown below
+START http://localhost/index.php
