90aa5ae17c7726e56469d72c4252664a924c7e29
Jayvee <jayveecastaneda19@gmail.com>
2021-06-23 16:34:45 +0800

    Improved Header for Help page

--- src/help.php
+++ src/help.php
@@ -1,11 +1,43 @@
-<?php include('header.php'); ?>
+<?php include("conn.php"); ?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<title>Zest - Help</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+	<!-- Stylesheets -->
+	<link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
+	<link href="css/global.css" rel="stylesheet">
+	<link href="css/home.css" rel="stylesheet">
+	<link href="css/overrides.css" rel="stylesheet">
+
+	<!-- Favicon -->
+	<link rel="icon" href="../resources/icons/lemon-icon.png">
+
+	<!-- Bootstrap JS and jQuery-->
+	<script src="js/jquery-3.5.1.min.js"></script>
+	<script src="bootstrap/js/bootstrap.bundle.min.js"></script>
+
+	<!-- Scripts -->
+	<script src="js/sidebar.js"></script>
+	<script src="js/moment.min.js"></script>
+	<script>
+		$(function() {
+			$("[data-toggle='tooltip']").tooltip()
+		})
+	</script>
+
+	<!-- Setting the default timezone for all PHP date() calls -->
+	<?php date_default_timezone_set("Asia/Manila"); ?>
+</head>
+
 <?php include('notification.php'); ?>
 <?php
 	session_start();
 	$default = $_GET['help'];
 	$_SESSION['default'] = $_GET['help'];
 ?>
-
 <body class="help-bg">
 	<nav class="navbar fixed-top navbar-dark bg-dark">
   		<a class="navbar-brand" href="../src">
