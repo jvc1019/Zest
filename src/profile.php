9b3d090b26f69ec04c4edca72ab7a19793d8cea4
Jayvee <jayveecastaneda19@gmail.com>
2021-06-23 22:58:21 +0800

    Improved Profile look

--- src/profile.php
+++ src/profile.php
@@ -10,7 +10,7 @@
 			<div class="card profileHolder mx-auto shadow p-3" style="width:1000px">
 				<div class="card-body">
 					<div class="row">
-						<div class="col-md-6">
+						<div class="col-md-7">
 							<div class="text-center">
 							<?php echo "<img class='card-img-top img-fluid center' src='../resources/avatars/$user_Avatar.jpg' style='width:250px' alt='Profile Avatar'>" ?>
 							</div><br>
@@ -22,20 +22,20 @@
 							</p>
 							<br>
 							<div class="row">
-								<div class="col-md-6">
+								<div class="col-md-4">
 									<button data-toggle="modal" data-target="#updateProfileModal<?php echo $user['user_ID']; ?>" class="btn btn-sm btn-primary btn-block">
 										<svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-gear-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
 				  						<path fill-rule="evenodd" d="M9.405 1.05c-.413-1.4-2.397-1.4-2.81 0l-.1.34a1.464 1.464 0 0 1-2.105.872l-.31-.17c-1.283-.698-2.686.705-1.987 1.987l.169.311c.446.82.023 1.841-.872 2.105l-.34.1c-1.4.413-1.4 2.397 0 2.81l.34.1a1.464 1.464 0 0 1 .872 2.105l-.17.31c-.698 1.283.705 2.686 1.987 1.987l.311-.169a1.464 1.464 0 0 1 2.105.872l.1.34c.413 1.4 2.397 1.4 2.81 0l.1-.34a1.464 1.464 0 0 1 2.105-.872l.31.17c1.283.698 2.686-.705 1.987-1.987l-.169-.311a1.464 1.464 0 0 1 .872-2.105l.34-.1c1.4-.413 1.4-2.397 0-2.81l-.34-.1a1.464 1.464 0 0 1-.872-2.105l.17-.31c.698-1.283-.705-2.686-1.987-1.987l-.311.169a1.464 1.464 0 0 1-2.105-.872l-.1-.34zM8 10.93a2.929 2.929 0 1 0 0-5.86 2.929 2.929 0 0 0 0 5.858z"/>
 										</svg> Edit Profile
 									</button>
 								</div>
-								<div class="col-md-6">
+								<div class="col-md-4">
 									<a href="change.php" class="btn btn-sm btn-secondary btn-block" role="button">
 										<svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-gear-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
 				  						<path fill-rule="evenodd" d="M9.405 1.05c-.413-1.4-2.397-1.4-2.81 0l-.1.34a1.464 1.464 0 0 1-2.105.872l-.31-.17c-1.283-.698-2.686.705-1.987 1.987l.169.311c.446.82.023 1.841-.872 2.105l-.34.1c-1.4.413-1.4 2.397 0 2.81l.34.1a1.464 1.464 0 0 1 .872 2.105l-.17.31c-.698 1.283.705 2.686 1.987 1.987l.311-.169a1.464 1.464 0 0 1 2.105.872l.1.34c.413 1.4 2.397 1.4 2.81 0l.1-.34a1.464 1.464 0 0 1 2.105-.872l.31.17c1.283.698 2.686-.705 1.987-1.987l-.169-.311a1.464 1.464 0 0 1 .872-2.105l.34-.1c1.4-.413 1.4-2.397 0-2.81l-.34-.1a1.464 1.464 0 0 1-.872-2.105l.17-.31c.698-1.283-.705-2.686-1.987-1.987l-.311.169a1.464 1.464 0 0 1-2.105-.872l-.1-.34zM8 10.93a2.929 2.929 0 1 0 0-5.86 2.929 2.929 0 0 0 0 5.858z"/>
 										</svg> Change password</a>
 								</div>
-								<div class="col-md-6">
+								<div class="col-md-4">
 									<button data-toggle="modal" data-target="#deleteAccountModal<?php echo $user['user_ID']; ?>" class="btn btn-sm btn-danger btn-block">
 				                    <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-x-circle" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
 				                        <path fill-rule="evenodd" d="M8 15A7 7 0 1 0 8 1a7 7 0 0 0 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z" />
@@ -46,7 +46,7 @@
 			                	</div>
 		                	</div>
 						</div>
-						<div class="col-md-6">
+						<div class="col-md-5">
 							<h5 class="text-center">Description</h5>
 							<div class="mx-auto font-italic<?php if (empty($user_Desc)) { echo ' text-center'; } else { echo ' text-justify'; } ?>">
 								<?php
