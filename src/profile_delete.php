f291bdd30948bc87c715581cd7d56a042639548f
Jayvee <jayveecastaneda19@gmail.com>
2021-06-23 22:07:45 +0800

    added session_destroy()

--- src/profile_delete.php
+++ src/profile_delete.php
@@ -12,6 +12,8 @@
     if ($user_Password == $RePass){
         $sqlDelete = "DELETE FROM `user` WHERE `user_ID`='$ID'";
     	$conn->query($sqlDelete);
+        
+        session_destroy();
         header("Location:landing.php?status_heading=Account Deleted&status=You have permanently deleted your account&type=notif");
     }else{
         header('location:profile.php?help=change&status_heading=Account Not Deleted&status=Password is incorrect&type=notif');
