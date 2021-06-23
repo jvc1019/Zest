25fc083988b57c2a2d5a70b34b34d11ac0b8f1ca
Adriel <jmrabe@up.edu.ph>
2021-06-23 21:40:38 +0800

    Added line in setup.cmd to create shortcut to the desktop

--- setup.cmd
+++ setup.cmd
@@ -7,4 +7,6 @@
 :: Copies the folder and subfolder extracted into htdocs of xampp. xampp must be installed
 ROBOCOPY /mir /mt:8 .\Zest C:\Zest
 XCOPY "Zest.lnk" "%UserProfile%\Desktop"
+XCOPY "Uninstall.lnk" "%UserProfile%\Desktop"
+XCOPY "Zest.lnk" "%ProgramData%\Microsoft\Windows\Start Menu\Programs\Zest"
 
