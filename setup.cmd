7cfa3ddc36b6f84afb90742d94d9b26abc1ecd32
Adriel <jmrabe@up.edu.ph>
2021-06-23 23:44:35 +0800

    Updated documentation for setup.cmd and finished self-extracting installer

--- setup.cmd
+++ setup.cmd
@@ -7,8 +7,10 @@
 :: Copies the folder and subfolders inside Zest
 ROBOCOPY /mir /mt:8 .\Zest C:\Zest
 
-::Copies shortcuts into the Desktop of the user
+::Copies shortcut to Desktop of the user
 XCOPY /i /y "Zest.lnk" "%UserProfile%\Desktop"
+
+::Creates a folder in start menu and places shortcuts in that directory
 MKDIR %AppData%\Microsoft\Windows\Start Menu\Programs\Zest
 XCOPY /i /y "Uninstall.lnk" "%AppData%\Microsoft\Windows\Start Menu\Programs\Zest"
 XCOPY /i /y "Zest.lnk" "%AppData%\Microsoft\Windows\Start Menu\Programs\Zest"
