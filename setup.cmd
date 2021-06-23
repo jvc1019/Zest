ace62cd1301acc3e1906af80c4408d5701aa9265
Adriel <jmrabe@up.edu.ph>
2021-06-23 21:46:51 +0800

    Fixed documentation in setup.cmd

--- setup.cmd
+++ setup.cmd
@@ -4,8 +4,10 @@
 ::  Special Thanks to Janley Molina  ::
 :::::::::::::::::::::::::::::::::::::::
 
-:: Copies the folder and subfolder extracted into htdocs of xampp. xampp must be installed
+:: Copies the folder and subfolders inside Zest
 ROBOCOPY /mir /mt:8 .\Zest C:\Zest
+
+::Copies shortcuts into the Desktop of the user
 XCOPY "Zest.lnk" "%UserProfile%\Desktop"
 XCOPY "Uninstall.lnk" "%UserProfile%\Desktop"
 XCOPY "Zest.lnk" "%ProgramData%\Microsoft\Windows\Start Menu\Programs\Zest"
