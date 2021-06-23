1d71f12a420c7e2c7a0b23b14e98ed5c3cb63ff6
Adriel <jmrabe@up.edu.ph>
2021-06-23 15:17:33 +0800

    changed target location for apache_start.bat of ZEST.cmd

--- Zest/ZEST.cmd
+++ Zest/ZEST.cmd
@@ -5,7 +5,7 @@
 :::::::::::::::::::::::::::::::::::::::
 
 :: Runs apache_start to access the website without the need to open xampp_control 
-START C:\Zest\apache_start.bat 
+START C:\Zest\Zest\apache_start.bat 
 
 :: Opens index.php located at the address shown below
 START http://localhost/index.php
